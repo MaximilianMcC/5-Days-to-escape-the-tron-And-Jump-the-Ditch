c172969a64e6c9bacbfcfc60f331fc6e368c19bc
MaximilianMcC <max@maximilian.co.nz>
2025-12-18 14:17:19 +1300

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿using Raylib_cs;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
+		Raylib.InitWindow(720, 480, "5 days to escape the tron and jump the ditch");
+
+		while (!Raylib.WindowShouldClose())
+		{
+			Raylib.BeginDrawing();
+			Raylib.ClearBackground(Color.Magenta);
+			Raylib.DrawText("5 days to escape the tron and jump the ditch", 10, 10, 30, Color.White);
+			Raylib.EndDrawing();
+		}
+
+		Raylib.CloseWindow();
+	}
+}
